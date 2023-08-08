cd33961bdd7716d9d1963b8f1ec6b4d340c6cc5d
syjoe <csy5501@gmail.com>
2023-08-08 11:18:56 +0900

    [added] options in database

--- lion_app/lion_app/settings.py
+++ lion_app/lion_app/settings.py
@@ -99,6 +99,9 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'db'),
+        'OPTIONS': {
+            'options': '-c search_path=likelion,public',
+        },
     }
 }
 
