77d943d271d3feb33a147f6d6406167dc287b522
syjoe <csy5501@gmail.com>
2023-08-11 11:20:24 +0900

    Forum.views: Fix schema

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -5,12 +5,14 @@ from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
-@extend_schema(tags=["Topics", "Forum"])
+@extend_schema(tags=["Topics"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
-
-
+    
+    @extend_schema(summary="Create a new topic")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
 @extend_schema(tags=["Post", "Forum"])
 class PostViewSet(viewsets.ModelViewset):
