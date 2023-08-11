f0ed3f3b6020359c1dbe07bec361ee1c74a8e9dd
syjoe <csy5501@gmail.com>
2023-08-11 11:18:18 +0900

    lesson3: edit post, forum

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -5,14 +5,14 @@ from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
-@extend_schema(tags=["Topics"])
+@extend_schema(tags=["Topics", "Forum"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
 
 
 
-@extend_schema(tags=["Post"])
+@extend_schema(tags=["Post", "Forum"])
 class PostViewSet(viewsets.ModelViewset):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
