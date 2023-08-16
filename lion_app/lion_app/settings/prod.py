32dc4e3306ee1d5c74037d7569787ef4eae04be1
syjoe <csy5501@gmail.com>
2023-08-16 11:10:01 +0900

    REFACTOR: settings

---
+++ lion_app/lion_app/settings/prod.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "lion-lb-18904324-86ca7c2cfe7d.kr.lb.naverncp.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://lion-lb-18904324-86ca7c2cfe7d.kr.lb.naverncp.com",
+]
