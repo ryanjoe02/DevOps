15b8bce7602a84106a9c1fbe177067559117c90c
syjoe <csy5501@gmail.com>
2023-08-22 15:54:27 +0900

    apply black

--- lion_app/lion_app/settings/staging.py
+++ lion_app/lion_app/settings/staging.py
@@ -5,7 +5,8 @@ from .base import *
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = [
-    "localhost" "223.130.139.108",
+    "localhost",
+    "223.130.139.108",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
