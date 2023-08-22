8b6bafb30ab0a1d8c3aedcfd2ac4b41412a8e13e
syjoe <csy5501@gmail.com>
2023-08-22 15:28:54 +0900

    feat: modify typo

--- lion_app/lion_app/settings/staging.py
+++ lion_app/lion_app/settings/staging.py
@@ -1,16 +1,19 @@
 import os
 
-from base import *
+from .base import *
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "lion-db-staging-18975816-75c69b2d58f5.kr.lb.naverncp.com",
+    "223.130.139.108:8000",
+    "lion-db-staging-18975816-75c69b2d58f5.kr.lb.naverncp.com",	
+
 ]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:8888",
-#     "http://lion-db-staging-18975816-75c69b2d58f5.kr.lb.naverncp.com",
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "223.130.139.108:8000",    
+    # "http://localhost:8888",
+    "http://lion-db-staging-18975816-75c69b2d58f5.kr.lb.naverncp.com",
+]
